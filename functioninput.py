e013cfbb873cccddeafe9591b58ff02671ec837e
perfectpitchpiano <ikarthik@eastsideprep.org>
2025-08-26 11:54:52 -0700

    Create functioninput.py

---
+++ functioninput.py
@@ -0,0 +1,6 @@
+def functioninput(): #creates function
+    user_input = input("Enter anything here:") #allows for user to enter anything they want in the function
+    return user_input #allows for "user_input" to be used outside of the function
+
+user_input = functioninput() #defines "user_input" as the function
+print(user_input) #prints the user input
