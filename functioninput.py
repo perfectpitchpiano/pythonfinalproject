142f963b17241dda5248b63140bd41c88de8766a
perfectpitchpiano <ikarthik@eastsideprep.org>
2025-08-27 14:50:50 -0700

    Update functioninput.py

--- functioninput.py
+++ functioninput.py
@@ -5,7 +5,7 @@ print(hello("Ivanka")) #function utilizing the argument "Ivanka"
 
 
 
-def multiply(a,b): #new function "multiply" that accepts the parameters "a" amd "b"
+def multiply(a,b): #new function "multiply" that accepts the parameters "a" and "b"
     return a*b #returns the output of the function
 
 print(multiply(2,3)) #function utilizing the arguments "2" and "3"
